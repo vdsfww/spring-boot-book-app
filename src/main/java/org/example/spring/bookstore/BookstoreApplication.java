50d538a4c24e6c759703de3f7eb0d2e081521793
vdsfww <loppprt@gmail.com>
2025-09-05 11:41:28 +0300

    fix

--- src/main/java/org/example/spring/bookstore/BookstoreApplication.java
+++ src/main/java/org/example/spring/bookstore/BookstoreApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class    BookstoreApplication {
+public class BookstoreApplication {
     @Autowired
     private BookService bookService;
 
